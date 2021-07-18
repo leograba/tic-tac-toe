42e94e2d6f3fa51607a9f68feb55a9eceee374e7
Leonardo Graboski Veiga <leogveiga@gmail.com>
2021-07-18 19:29:17 -0300

    app: add OpenGL ES context to the SDL initialization
    Make the calls so SDL renderer uses OpenGL and hints for OpenGL ES.

--- src/CApp_OnEvent.cpp
+++ src/CApp_OnEvent.cpp
@@ -1,8 +1,10 @@
 #include "CApp.h"
+#include <iostream>
 
 void CApp::OnEvent(SDL_Event *Event) {
     if (Event->type == SDL_QUIT)
     {
+        std::cout << "Exiting..." << std::endl;
         Running = false;
     }
 }
--- src/CApp_OnInit.cpp
+++ src/CApp_OnInit.cpp
@@ -1,4 +1,6 @@
 #include "CApp.h"
+#include <iostream>
+
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 
@@ -6,36 +8,58 @@ bool CApp::OnInit() {
     int rendererFlags, windowFlags;
 
     rendererFlags = SDL_RENDERER_ACCELERATED;
+    windowFlags = SDL_WINDOW_OPENGL;
 
-    windowFlags = 0;
-
+    //initialize SDL
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
     {
-        printf("Couldn't initialize SDL: %s\n", SDL_GetError());
+        std::cerr << "Couldn't initialize SDL: " << SDL_GetError() << "\n";
         return false;
     }
 
-    window = SDL_CreateWindow("CApp 01", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                              SCREEN_WIDTH, SCREEN_HEIGHT, windowFlags);
+    std::cout << "SDL_GetCurrentVideoDriver: " << SDL_GetCurrentVideoDriver()
+        << "\n";
+
+    //OpenGL context to use OpenGL ES
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    //create window
+    window = SDL_CreateWindow("CApp 01", SDL_WINDOWPOS_CENTERED,
+                              SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH,
+                              SCREEN_HEIGHT, windowFlags);
 
     if (!window)
     {
-        printf("Failed to open %d x %d window: %s\n", SCREEN_WIDTH, SCREEN_HEIGHT, SDL_GetError());
+        std::cerr << "Failed to open window: " << SDL_GetError() << "\n";
         exit(1);
     }
 
+    //Suggest OpenGL ES 2
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+    SDL_SetHint(SDL_HINT_RENDER_DRIVER, "opengles2");
 
+    //create renderer
     renderer = SDL_CreateRenderer(window, -1, rendererFlags);
 
     if (!renderer)
     {
-        printf("Failed to create renderer: %s\n", SDL_GetError());
+        std::cerr << "Failed to create renderer: " << SDL_GetError() << "\n";
         exit(1);
     }
 
-    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00,
-                           SDL_ALPHA_OPAQUE);
+    //Render something
+    SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    //Set colour of renderer
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
+
+    //Clear the screen to the set colour
+    SDL_RenderClear(renderer);
+
+    //Show all the has been done behind the scenes
+    SDL_RenderPresent(renderer);
 
     return true;
 }
